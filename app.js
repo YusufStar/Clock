2470d5436f765447bc30b0e483ae83966aa1fd79
Yusuf Yıldız <ysfyldz0077@gmail.com>
2022-09-19 21:38:00 +0300

    .

--- app.js
+++ app.js
@@ -1,59 +1,24 @@
-var myhour, myminute, mysecond;
-
-function flipNumber(el, newnumber) {
-  var thistop = el.find(".top").clone();
-  var thisbottom = el.find(".bottom").clone();
-  thistop.addClass("new");
-  thisbottom.addClass("new");
-  thisbottom.find(".text").text(newnumber);
-  el.find(".top").after(thistop);
-  el.find(".top.new").append(thisbottom);
-  el.addClass("flipping");
-  el.find(".top:not(.new)").find(".text").text(newnumber);
-  setTimeout(function () {
-    el.find(".bottom:not(.new)").find(".text").text(newnumber);
-  }, 500);
-}
-function setTime() {
-  $(".flipper").removeClass("flipping");
-  $(".flipper .new").remove();
-  var date = new Date();
-  var seconds = date.getSeconds().toString();
-  if (seconds.length == 1) {
-    seconds = "0" + seconds;
+var clock = new Vue({
+  el: '#clock',
+  data: {
+      time: '',
+      date: ''
   }
-  var minutes = date.getMinutes().toString();
-  if (minutes.length == 1) {
-    minutes = "0" + minutes;
-  }
-  var hour = date.getHours();
-  if (hour > 12) {
-    hour = hour - 12;
-  }
-  if (hour == 0) {
-    hour = 12;
-  }
-  hour = hour.toString();
-  if (hour.length == 1) {
-    hour = "0" + hour;
-  }
-  if ($(myhour[0]).text() !== hour) {
-    flipNumber($(myhour[0]).closest(".flipper"), hour);
-  }
-  if ($(myminute[0]).text() !== minutes) {
-    flipNumber($(myminute[0]).closest(".flipper"), minutes);
-  }
-  if ($(mysecond[0]).text() !== seconds) {
-    flipNumber($(mysecond[0]).closest(".flipper"), seconds);
-  }
-  setTimeout(function () {
-    setTime();
-  }, 500);
-}
-
-(function () {
-  myhour = $(".clock .flipper:nth-child(1) div:not(.new) .text");
-  myminute = $(".clock .flipper:nth-child(2) div:not(.new) .text");
-  mysecond = $(".clock .flipper:nth-child(3) div:not(.new) .text");
-  setTime();
 });
+
+var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+var timerID = setInterval(updateTime, 1000);
+updateTime();
+function updateTime() {
+  var cd = new Date();
+  clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+  clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+};
+
+function zeroPadding(num, digit) {
+  var zero = '';
+  for(var i = 0; i < digit; i++) {
+      zero += '0';
+  }
+  return (zero + num).slice(-digit);
+}
